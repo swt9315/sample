7cd2e4b88b6a32a1317b12ac367167cc056713f7
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-04 22:07:38 +0900

    コミット2

--- sample.rb
+++ sample.rb
@@ -1,5 +1,13 @@
-def hello
-  p "hello, GitHub Desktop."
+
+
+def in1to10(num, outside_mode)
+  if (num >= 1 && num <= 10) || outside_mode
+    puts "True"
+  else
+    puts "False"
+  end
 end
 
-hello
+puts "数値を入力してください"
+num = gets.to_i
+outside_mode = gets.to_itoha
